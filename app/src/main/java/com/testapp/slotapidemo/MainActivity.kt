4249cb979c6684f23be1e36abec50ca4ab52ce4d
Adedoyin <isradeoluwa@gmail.com>
2024-10-07 23:07:21 +0100

    ScreenContent created

--- app/src/main/java/com/testapp/slotapidemo/MainActivity.kt
+++ app/src/main/java/com/testapp/slotapidemo/MainActivity.kt
@@ -3,6 +3,8 @@ package com.testapp.slotapidemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.testapp.slotapidemo.ui.theme.SlotApiDemoTheme
@@ -26,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                   MainScreen()
+                    MainScreen()
                 }
             }
         }
@@ -41,11 +44,27 @@ fun MainScreen() {
 
     var imageSelected by remember { mutableStateOf(true) }
 
-    val onLinearClick = {
-        value : Boolean -> linearSelected = value
+    val onLinearClick = { value: Boolean ->
+        linearSelected = value
     }
 
-    val onTitleClick = {
-        value : Boolean -> imageSelected = value
+    val onTitleClick = { value: Boolean ->
+        imageSelected = value
+    }
+}
+
+@Composable
+fun ScreenContent(
+    linearSelected: Boolean,
+    imageSelected: Boolean,
+    onTitleClick: (Boolean) -> Unit,
+    onLinearClick: (Boolean) -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+
     }
 }
