29822279257c59f53d877ed1a3f0003fb49c7407
Adedoyin <isradeoluwa@gmail.com>
2024-10-07 23:00:01 +0100

    MainScreen Composable created

--- app/src/main/java/com/testapp/slotapidemo/MainActivity.kt
+++ app/src/main/java/com/testapp/slotapidemo/MainActivity.kt
@@ -8,6 +8,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.testapp.slotapidemo.ui.theme.SlotApiDemoTheme
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   MainScreen()
                 }
             }
         }
@@ -30,17 +34,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
+    var linearSelected by remember {
+        mutableStateOf(true)
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SlotApiDemoTheme {
-        Greeting("Android")
+    var imageSelected by remember { mutableStateOf(true) }
+
+    val onLinearClick = {
+        value : Boolean -> linearSelected = value
+    }
+
+    val onTitleClick = {
+        value : Boolean -> imageSelected = value
     }
 }
