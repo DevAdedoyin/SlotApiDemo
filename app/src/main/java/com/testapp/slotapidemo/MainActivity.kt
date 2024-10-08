5af18876cbc88a8f60cca49b1762d3e3791dede7
Adedoyin <isradeoluwa@gmail.com>
2024-10-08 01:37:58 +0100

    Slot API Demo Completed

--- app/src/main/java/com/testapp/slotapidemo/MainActivity.kt
+++ app/src/main/java/com/testapp/slotapidemo/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import android.widget.CheckBox
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -22,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.testapp.slotapidemo.ui.theme.SlotApiDemoTheme
@@ -58,6 +64,29 @@ fun MainScreen() {
     val onTitleClick = { value: Boolean ->
         imageSelected = value
     }
+
+    ScreenContent(
+        linearSelected = linearSelected,
+        imageSelected = imageSelected,
+        onTitleClick = onTitleClick,
+        onLinearClick = onLinearClick,
+        titleContent = {
+            if (imageSelected) {
+                TitleImage(drawing = R.drawable.baseline_cloud_download_24)
+            } else {
+                Text(
+                    text = "Downloading",
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier.padding(30.dp)
+                )
+            }
+        }, progressContent = {
+            if (linearSelected) {
+                LinearProgressIndicator(Modifier.height(40.dp))
+            } else {
+                CircularProgressIndicator(Modifier.size(200.dp), strokeWidth = 18.dp)
+            }
+        })
 }
 
 @Composable
@@ -65,14 +94,23 @@ fun ScreenContent(
     linearSelected: Boolean,
     imageSelected: Boolean,
     onTitleClick: (Boolean) -> Unit,
-    onLinearClick: (Boolean) -> Unit
+    onLinearClick: (Boolean) -> Unit,
+    titleContent: @Composable () -> Unit,
+    progressContent: @Composable () -> Unit
 ) {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
-
+        titleContent()
+        progressContent()
+        CheckBoxes(
+            linearSelected = linearSelected,
+            imageSelected = imageSelected,
+            onTitleClick = onTitleClick,
+            onLinearClick = onLinearClick,
+        )
     }
 }
 
@@ -90,8 +128,18 @@ fun CheckBoxes(
         )
         Text(text = "Image Title")
         Spacer(modifier = Modifier.width(20.dp))
-        Checkbox(checked = linearSelected, onCheckedChange = onTitleClick)
+        Checkbox(checked = linearSelected, onCheckedChange = onLinearClick)
         Text(text = "Linear Progress")
     }
 }
 
+@Composable
+fun TitleImage(drawing: Int) {
+    Image(painter = painterResource(id = drawing), contentDescription = "title image")
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun DemoPreview() {
+    MainScreen()
+}
